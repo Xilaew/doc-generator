13a0039a702906828873b995d121082efda7eac4
Felix von Poblotzki <xilaew@gmail.com>
2019-04-14 22:40:04 +0200

    added some usefull pandoc calls to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ./docgen.py
-pandoc -N --pdf-engine=xelatex --toc -o example14.pdf
-pandoc template_exp.md -o simple.pdf
-#pdflatex simple.tex
-evince simple.pdf
+pandoc -s --toc --variable lof --variable lot template_exp.md -o template_exp.tex
+pandoc -s --toc template_exp.md -o template_exp.html
+pandoc --toc template_exp.md -o template_exp.docx
+pandoc template_exp.docx -o template_exp.docx.pdf
+xelatex template_exp.tex
+evince template_exp.pdf
+evince template_exp.docx.pdf
