ea797cfa9aa3169ba6093a9d49ae9c9322812961
Felix von Poblotzki <xilaew@gmail.com>
2019-04-15 00:00:24 +0200

    added latexmk and multirun pdflatex

--- run.sh
+++ run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+rm -f template_exp*
 ./docgen.py
+
+pandoc -s --toc --variable lof --variable lot template_exp.md -o template_exp.pdf
 pandoc -s --toc --variable lof --variable lot template_exp.md -o template_exp.tex
+ln --symbolic template_exp.tex template_exp.xetex.tex
+ln --symbolic template_exp.tex template_exp.pdftex.tex
+latexmk -quiet -xelatex template_exp.xetex.tex
+pdflatex template_exp.pdftex.tex
+pdflatex template_exp.pdftex.tex
+
 pandoc -s --toc template_exp.md -o template_exp.html
+
 pandoc --toc template_exp.md -o template_exp.docx
 pandoc template_exp.docx -o template_exp.docx.pdf
-xelatex template_exp.tex
-evince template_exp.pdf
-evince template_exp.docx.pdf
+
+evince template_exp.pdf &
+evince template_exp.xetex.pdf &
+evince template_exp.pdftex.pdf &
+evince template_exp.docx.pdf &
+firefox template_exp.html &
